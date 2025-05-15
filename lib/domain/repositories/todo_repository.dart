6f384307f46f7d5742976ffd003c903b062a796c
Preeti Ahuja <72223546+preetiahuja18@users.noreply.github.com>
2025-05-15 21:22:45 +0530

    Update todo_repository.dart

--- lib/domain/repositories/todo_repository.dart
+++ lib/domain/repositories/todo_repository.dart
@@ -3,6 +3,6 @@ import 'package:task_bloc/domain/entities/todo.dart';
 abstract class TodoRepository {
   Future<void> addTodo(Todo todo);
   Future<void> deleteTodo(Todo todo);
-  Future<void> updateTodo(int index, Todo todo); // <-- change this
+  Future<void> updateTodo(int index, Todo todo); 
   Future<List<Todo>> getTodos();
 }
